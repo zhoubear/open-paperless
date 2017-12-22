a7551224fc5fdb3054dfd773c5b6b44cd59f5382
Guillaume RODRIGUEZ <guiguidu31300@gmail.com>
2017-12-22 02:40:19 -0500

    Add Docker configuration

--- mayan/settings/staging/docker.py
+++ mayan/settings/staging/docker.py
@@ -1,16 +1,17 @@
 from __future__ import absolute_import
+import os
 
 from ..production import *  # NOQA
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DATABASE_NAME', 'postgres'),
+        'USER': os.getenv('DATABASE_USER', 'postgres'),
+        'HOST': os.getenv('DATABASE_HOST', 'localhost'),
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
 }
 
-BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+BROKER_URL = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/0')
