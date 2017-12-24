763678c46d8f7861a2238b652de858c3170be43b
Robin Naundorf <r.naundorf@fh-muenster.de>
2017-12-24 16:33:46 +0100

    Update apt cache before installing packages
    Signed-off-by: Robin Naundorf <r.naundorf@fh-muenster.de>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo apt-get update
+
 sudo apt-get install virtualenvwrapper graphviz redis-server \
 libpq-dev libjpeg-dev libmagic1 libpng-dev libreoffice \
 libtiff-dev gcc ghostscript gnupg python-dev python-virtualenv \
